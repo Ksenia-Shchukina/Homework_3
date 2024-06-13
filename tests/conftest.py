c8d10a957481cc88b60f41de768e4672e1c6be17
Ksenia_Shchukina <schukina.ksenya.s@gmail.com>
2024-06-13 17:53:01 +0300

    Fix in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,11 @@ import pytest
 
 from selene import browser
 
-browser.config.base_url = 'https://google.com'
-
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_for_test():
+    browser.config.base_url = 'https://google.com'
     browser.config.window_width = '1920'
     browser.config.window_height = '1080'
-    browser.open('')
     yield
     browser.quit()
--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -2,10 +2,16 @@ from selene import browser, have
 
 
 class TestGoogleSearch:
-    def test_google_search_valid_input(self, browser_for_test):
+    def test_google_search_valid_input(self):
+        browser.open('')
         browser.element('[name="q"]').type('qa guru').press_enter()
         browser.element('[class="notranslate HGLrXd NJjxre iUh30 ojE3Fb"]').should(have.text('QA.GURU'))
 
-    def test_google_search_invalid_input(self, browser_for_test):
+    def test_google_search_invalid_input(self):
+        browser.open('')
         browser.element('[name="q"]').type('blablabla muhaha 123456qwertydddddddddddddddddddddddddd').press_enter()
-        browser.element('[style="padding-top:.33em"]').should(have.text('не найдены.'))
+        browser.element('#rcnt').should(
+            have.text(
+                'По запросу blablablamuhaha123456qwertydddddddddddddddddddddddddd ничего не найдено'
+            )
+        )
